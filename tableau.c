eb4ec682ec5b94597d6737a92cefa668781e863d
laureannehenaff <71849436+laureannehenaff@users.noreply.github.com>
2020-10-09 13:53:13 +0200

    Add files via upload

--- tableau.c
+++ tableau.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define NBMAXNOTES 2 //On généralise l'effectif
+#include <math.h>
+#define carre(x) (x) * (x)
 
 int sortie = 0;
 int nombre;
@@ -11,17 +14,29 @@ int notemin;
 int notemax;
 
 int notes[29];
+int j;
+
+float ecart_type;
+float coefficient;
+float somme;
 
 int main() {
-    for (int i = 0 ; i<=29 ; i++) {  //programme tourne pour 30 notes
-        printf("Entrer la note  no %d :",i);
+    for (int l = 0; l<=NBMAXNOTES-1; l++) {
+        notes[l]=-2;                    //On initialise le tableau avec -2
+        printf("\n %i",notes[l]);
+    }
+    for (int i = 1 ; i<=NBMAXNOTES ; i++) {  //programme tourne pour 30 notes
+        printf("\n Entrer la note  no %d :",i);
         scanf("%f",&note);
+        j=i-1;
+        notes[j]=note;
         if (note<0 || note>20) {
             printf("\nÉlève absent ? Ou voulez-vous arrêter la saisie des notes ? A/O/N");
             scanf("%s",&saisie);
             if (saisie == 'A') { 
                 printf("\nL'élève no %d est absent",i);     //Absence pas de note
                 compteurabsences = compteurabsences + 1;
+                notes[j]=-1;
             }
             if (saisie == 'O') {                   //Sortie du programme
                 break; 
@@ -30,22 +45,34 @@ int main() {
                 printf("\nRetapez la note %d",i);
                 scanf("%2f",&note);
                 totalnotes = totalnotes + note;
+                notes[j]=note;
             }
         }
         totalnotes = totalnotes + note;
-        if (i == 0) {
+        if (i == 1) {
             notemin = note;             //On initialise notemin et notemax avec la première note rentrée
             notemax = note;
         }
-        if (i>0 && notemin>note) {     //On change notemin pour obtenir la plus petite note
-            notemin=note;
+        if (i>1 && notemin>note) {     //On change notemin pour obtenir la plus petite note
+            notemin = note;
         }
-        if (i>0 && notemax<note) {      //On change notemax pour obtenir la plus grande note
-            notemax=note;
+        if (i>1 && notemax<note) {      //On change notemax pour obtenir la plus grande note
+            notemax = note;
         }
     }
-    printf("\n Il y a %d notes valides",30-compteurabsences);
+    printf("\n Il y a %d notes valides",NBMAXNOTES-compteurabsences);
     printf("\n Il y %d absences",compteurabsences);
-    printf("\n La moyenne des notes valides est %d",totalnotes/(30-compteurabsences));
-    printf("\n La plus petite note est %d et la plus grande est %d \n",notemin,notemax);
+    printf("\n La moyenne des notes valides est %d",totalnotes/(NBMAXNOTES-compteurabsences));
+    printf("\n La plus petite note est %d et la plus grande est %d",notemin,notemax);
+    
+    coefficient=1/(NBMAXNOTES-compteurabsences-1);
+    for (int m = 0; m<=NBMAXNOTES-1; m++) {                     //on décompose le calcul de l'écart-type
+        somme= somme + carre((notes[m]-totalnotes/(NBMAXNOTES-compteurabsences)));
+    }
+    ecart_type=sqrt(coefficient*somme);   //somme=somme des (note - moyenne)^2
+    printf("\n L'écart-type est de %2f",ecart_type);
+
+    for (int k = 0; k<=NBMAXNOTES-1; k++) {     //On lit le tableau de notes
+        printf("\n %d \n",notes[k]);
+    }
 }
